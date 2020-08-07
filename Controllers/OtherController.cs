c2706ad7dd8d902db26ebd9762ada5bf0ea5ce99
Shoukox <58671746+Shoukox@users.noreply.github.com>
2020-08-08 01:45:30 +0500

    update

---
+++ Controllers/OtherController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication2.Controllers
+{
+    public class OtherController : Controller
+    {
+        public IActionResult Calc(double? first, string? chr, double? second)
+        {
+            string answer = "";
+            if(first != null && chr != null && second != null)
+            {
+                switch (chr)
+                {
+                    case "Сложение":
+                        answer = $"{first} + {second} = {Convert.ToDouble(first) + Convert.ToDouble(second)}";
+                        break;
+                    case "Вычитание":
+                        answer = $"{first} - {second} = {Convert.ToDouble(first) - Convert.ToDouble(second)}";
+                        break;
+                    case "Деление":
+                        answer = $"{first} / {second} = {Convert.ToDouble(first) / Convert.ToDouble(second)}";
+                        break;
+                    case "Умножение":
+                        answer = $"{first} * {second} = {Convert.ToDouble(first) * Convert.ToDouble(second)}";
+                        break;
+                    case "Степень":
+                        answer = $"Число {first} в степени {second} равняется {Math.Pow(Convert.ToDouble(first), Convert.ToDouble(second))}";
+                        break;
+                    case "Корень":
+                        answer = $"Число {first} в корне равняется: {Math.Sqrt(Convert.ToDouble(first))}";
+                        break;
+                }
+                ViewBag.Answer = answer;
+            }
+            return View();
+        }
+    }
+}
---
+++ Data/DataBase.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication2.Data.Models;
+
+namespace WebApplication2.Data
+{
+    public class DataBase : DbContext
+    {
+        public DataBase(DbContextOptions<DataBase> options) : base(options)
+        {
+
+        }
+        public DbSet<debils> debils { get; set; }
+        public DbSet<debilscats> debilscat { get; set; }
+    }
+}
